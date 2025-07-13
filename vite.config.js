4cfcc2428837fe30b1fec4ec075188d56abfe6f1
Will Alston <faradaybach@gmail.com>
2025-07-13 17:55:29 -0400

    Fix vite config (CommonJS)

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+/** vite.config.js  – CommonJS version */
+module.exports = {
+  base: './',          // ensures Capacitor loads files locally
+  build: {
+    outDir: 'dist',
+    emptyOutDir: true
+  }
+};
